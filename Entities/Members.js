644f3f4da1db9263005375bdf31b1b6adbbedde0
Luis <luis.ortiz.rivera@alumnos.upm.es>
2023-10-20 20:23:01 +0300

    Added logger to csv file

---
+++ Entities/Members.js
@@ -0,0 +1,19 @@
+const members = [
+	{
+		id: 1,
+		name: "John Doe",
+		email: "johnjonny@does.com",
+	},
+	{
+		id: 2,
+		name: "Amber stinger",
+		email: "stingyStinger@does.com",
+	},
+	{
+		id: 3,
+		name: "Rosa Melano",
+		email: "rosaRosita@does.com",
+	},
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const UAParser = require("ua-parser-js");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const members = require("./Entities/Members");
 
-// app.get("/", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+const logger = (req, res, next) => {
+	const date = new Date();
+	const logFileName = `log_${date.toISOString().split("T")[0]}.csv`;
+	const logFilePath = path.join(__dirname, "RequestLogs", logFileName);
 
-// app.get("/about", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "public", "about.html"));
-// });
+	// Get IP
+	let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+	// Get AGENT
+	let agent = new UAParser(req.headers["user-agent"]).getResult();
+
+	// Prepare the data in CSV format
+	const logData = `${date.toLocaleTimeString()},${req.protocol}://${req.get(
+		"host"
+	)}${req.originalUrl},${ip},${agent.os.name},${agent.browser.name}\n`;
+
+	// Write to log file
+	if (!fs.existsSync(logFilePath)) {
+		fs.writeFileSync(logFilePath, "Time,URL,IP,OS,Browser\n");
+	}
+	fs.appendFileSync(logFilePath, logData);
+
+	next();
+};
+
+//init middleware
+app.use(logger);
+
+app.get("/api/members", (req, res) => res.json(members));
 
 app.use(express.static(path.join(__dirname, "public")));
 
